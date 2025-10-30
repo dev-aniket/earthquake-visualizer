62cc0428d09a71236e8dafae48dae03e9ebff538
Aniket Mallick <aniketmallick@192.168.1.8>
2025-10-30 16:06:12 +0530

    cors update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,10 @@ const app = express();
 
 // MIDDLEWAES
 app.use(cors({
-  origin: 'http://localhost:5173',  // frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://earthquake-visualizer-phi.vercel.app"
+  ],
 }));
 
 
